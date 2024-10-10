ccd38db8177d27af5f772025af8bf0f2de6db59c
Bitcode1134 <154346752+Bitcode1134@users.noreply.github.com>
2024-10-10 12:44:30 -0400

    2

--- list.js
+++ list.js
@@ -1,7 +1,6 @@
 <html lang="en">
-    <button class="button" onClick="open(funiknight.netlify.app)">funiknight</button>
-    <button class="button" onClick="open(funiknight2.netlify.app)">funiknight2</button>
-    <button class="button" onClick="open(https://funispacecompany.netlify.app/)">funiknight</button>
-    <button class="button" onClick="open(funigames.netlify.app)">funiknight</button>
-
+    <button class="button" onClick="open(funiknight.netlify.app)">funiknight</button>,
+    <button class="button" onClick="open(funiknight2.netlify.app)">funiknight2</button>,
+    <button class="button" onClick="open(https://funispacecompany.netlify.app/)">funispacecompany</button>,
+    <button class="button" onClick="open(funigames.netlify.app)">funigames</button>,
 </html>
